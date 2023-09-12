c4271eb40cfd4b8280cca6621e4096e710ce56f0
Vikash Kumar <vikasthakur9310@gmail.com>
2023-09-12 23:54:31 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import Chart from 'chart.js/auto';
-import { CategoryScale } from 'chart.js/auto';
-import { useState } from 'react';
-import { Data } from './utils/Data';
-import './App.css';
-Chart.register((CategoryScale));
 
-function App() {
+
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+import { useState } from "react";
+import { Data } from "./utils/Data";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import './App.css';
+
+Chart.register(CategoryScale);
+
+export default function App() {
   const [chartData, setChartData] = useState({
     labels: Data.map((data) => data.year),
+
     datasets: [
       {
-        label: "Users Gained",
+        label: "Users Gained ",
         data: Data.map((data) => data.userGain),
         backgroundColor: [
           "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#f0331a",
+          "#f3ba2f",
+          "#2a71d0"
         ],
         borderColor: "black",
-        borderWidth: 2,
+        borderWidth: 2
       }
     ]
   });
+
   return (
     <div className="App">
-      <p>Using Chart.js in React</p>
+      <BarChart chartData={chartData} />
+      <LineChart chartData={chartData} />
+      <PieChart chartData={chartData} />
     </div>
   );
 }
-
-export default App;
